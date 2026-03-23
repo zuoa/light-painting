b45315562d4e6d6717f15ffdb7445077aee751a6
yujian <yujian@airimos.com>
2026-03-23 17:02:18 +0800

    1

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
-import type { NextConfig } from 'next'
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   // 纯前端静态导出，无需服务器
   output: 'export',
   distDir: 'dist',
@@ -11,4 +10,4 @@ const nextConfig: NextConfig = {
   },
 }
 
-export default nextConfig
+module.exports = nextConfig
